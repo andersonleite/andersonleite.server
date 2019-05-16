34fe944433666b27ec2ffba46ae8362c71ef5a32
Anderson Leite <anderson_leite@apple.com>
2019-05-16 16:30:24 -0700

    Post model.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  posts: forwardTo('db'),
+  // async posts(parent, args, ctx, info){
+  //   const posts = ctx.db.query.posts();
+  //   return posts;
+  // }
+};
 
 module.exports = Query;
